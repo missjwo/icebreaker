cb60ddcf34964d3247d5f40a8d0ab4c76b4e1ae3
Jenny Wong <jenny@humanmade.com>
2020-06-13 21:19:42 +0100

    - Add ability to pick a new random question in gutenberg block

--- src/block/block.js
+++ src/block/block.js
@@ -9,6 +9,8 @@ const { __ } = wp.i18n; // Import __() from wp.i18n
 
 import attributes from './attributes';
 import apiFetch from '@wordpress/api-fetch';
+import { Toolbar, Button } from '@wordpress/components';
+import { BlockControls } from '@wordpress/block-editor';
 
 const { registerBlockType } = wp.blocks; // Import registerBlockType() from wp.blocks
 // Import the element creator function (React abstraction layer)
@@ -20,12 +22,21 @@ const iconEl = el('svg', { width: 24, height: 24, viewBox:"0 0 100 100" },
   el('path', { d: "M97.5,50c0,1.7-1.4,3.1-3.1,3.1h-8l4,4c1.2,1.2,1.2,3.2,0,4.4c-0.6,0.6-1.4,0.9-2.2,0.9c-0.8,0-1.6-0.3-2.2-0.9l-8.4-8.4  H57.4l14.3,14.3h11.9c1.7,0,3.1,1.4,3.1,3.1s-1.4,3.1-3.1,3.1H78l5.6,5.6c1.2,1.2,1.2,3.2,0,4.4c-0.6,0.6-1.4,0.9-2.2,0.9  s-1.6-0.3-2.2-0.9L73.6,78v5.7c0,1.7-1.4,3.1-3.1,3.1s-3.1-1.4-3.1-3.1V71.8L53.1,57.4v20.1l8.4,8.4c1.2,1.2,1.2,3.2,0,4.4  c-0.6,0.6-1.4,0.9-2.2,0.9c-0.8,0-1.6-0.3-2.2-0.9l-4-4v8.2c0,1.7-1.4,3.1-3.1,3.1s-3.1-1.4-3.1-3.1v-8.2l-4,4  c-0.6,0.6-1.4,0.9-2.2,0.9s-1.6-0.3-2.2-0.9c-1.2-1.2-1.2-3.2,0-4.4l8.4-8.4v-20L32.7,71.8v11.9c0,1.7-1.4,3.1-3.1,3.1  c-1.7,0-3.1-1.4-3.1-3.1V78l-5.6,5.6c-0.6,0.6-1.4,0.9-2.2,0.9c-0.8,0-1.6-0.3-2.2-0.9c-1.2-1.2-1.2-3.2,0-4.4l5.6-5.6h-5.7  c-1.7,0-3.1-1.4-3.1-3.1s1.4-3.1,3.1-3.1h11.9l14.3-14.3H22.5l-8.4,8.4c-0.6,0.6-1.4,0.9-2.2,0.9c-0.8,0-1.6-0.3-2.2-0.9  c-1.2-1.2-1.2-3.2,0-4.4l4-4H5.6c-1.7,0-3.1-1.4-3.1-3.1s1.4-3.1,3.1-3.1h8.2l-4-4c-1.2-1.2-1.2-3.2,0-4.4c1.2-1.2,3.2-1.2,4.4,0  l8.4,8.4h20.1L28.4,32.7H16.5c-1.7,0-3.1-1.4-3.1-3.1c0-1.7,1.4-3.1,3.1-3.1h5.7l-5.8-5.8c-1.2-1.2-1.2-3.2,0-4.4  c1.2-1.2,3.2-1.2,4.4,0l5.8,5.8v-5.7c0-1.7,1.4-3.1,3.1-3.1c1.7,0,3.1,1.4,3.1,3.1v11.9l14.2,14.2V22.3l-8.4-8.4  c-1.2-1.2-1.2-3.2,0-4.4c1.2-1.2,3.2-1.2,4.4,0l4,4v-8c0-1.7,1.4-3.1,3.1-3.1s3.1,1.4,3.1,3.1v8l4-4c1.2-1.2,3.2-1.2,4.4,0  c1.2,1.2,1.2,3.2,0,4.4l-8.4,8.4v20.4l14.3-14.3V16.5c0-1.7,1.4-3.1,3.1-3.1s3.1,1.4,3.1,3.1v5.7l5.8-5.8c1.2-1.2,3.2-1.2,4.4,0  c1.2,1.2,1.2,3.2,0,4.4L78,26.5h5.7c1.7,0,3.1,1.4,3.1,3.1c0,1.7-1.4,3.1-3.1,3.1H71.8L57.6,46.9h20.1l8.4-8.4  c1.2-1.2,3.2-1.2,4.4,0c1.2,1.2,1.2,3.2,0,4.4l-4,4h8C96.1,46.9,97.5,48.3,97.5,50z" } )
 );
 
-function getRandomIntInclusive(min, max) {
+function getRandomIntInclusive( min, max ) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
 }
 
+function getNewQuestion( props ) {
+	const { setAttributes } = props;
+
+	apiFetch( { path: '/wp/v2/icebreakers' } )
+		.then( ( response ) => {
+			const i = getRandomIntInclusive( 0, response.length - 1 );
+			setAttributes( { randomQuestion: response[i].title.rendered } );
+		} );
+}
 /**
  * Register: Random icebreaker question Gutenberg Block.
  *
@@ -64,20 +75,27 @@ registerBlockType( 'jw/block-icebreaker', {
 	 * @returns {Mixed} JSX Component.
 	 */
 	edit: ( props ) => {
-		const { setAttributes } = props;
 
 		if( props.attributes.randomQuestion === undefined ){ 
-			apiFetch( { path: '/wp/v2/icebreakers' } )
-				.then( ( response ) => {
-					const i = getRandomIntInclusive( 0, response.length - 1 );
-					setAttributes( { randomQuestion: response[i].title.rendered } );
-				} );
+			getNewQuestion( props );
 		}
-		
+
 		return (
-			<div className={ props.className }>
-				<p>{props.attributes.randomQuestion}</p>
-			</div>
+			<React.Fragment>
+				<BlockControls>
+					<Toolbar>
+						<Button
+							label='Pick a new random question'
+							icon='controls-repeat'
+							className='icebreaker-refresh-button'
+							onClick={() => getNewQuestion( props )}
+						/>
+					</Toolbar>
+				</BlockControls>
+				<div className={ props.className }>
+					<p>{props.attributes.randomQuestion}</p>
+				</div>
+			</React.Fragment>
 		);
 	},
 
